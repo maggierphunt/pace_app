3696f53863b4f8ef5f8f65ae7be59bc5da37f070
Maggie Hunt <50377783+maggierphunt@users.noreply.github.com>
2021-05-10 18:47:59 +0100

    update

--- pace_app.py
+++ pace_app.py
@@ -1,23 +1,49 @@
-
 from flask import Flask, render_template, request, Response
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2  import SpotifyClientCredentials
+import os
+from os import getenv
+import argparse
+import logging
+ 
+#Spotify params
+spotify_client_id = os.getenv('SPOTIPY_CLIENT_ID', 'client_id')
+spotify_secret = os.getenv('SPOTIPY_CLIENT_SECRET', 'secret')
+spotify_redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI', 'redirect_uri')
+spotify_api_url = os.getenv('api_url', 'api_url')
+ 
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+items=[]
+ 
+logger = logging.getLogger('examples.create_playlist')
+logging.basicConfig(level='DEBUG')
+ 
 
+
+
+#logger = logging.getLogger('examples.create_playlist')
+#logging.basicConfig(level='DEBUG')
+
+
+###################
 app = Flask("pace_app") #making an app
-
+       
 #About
-@app.route("/about")  
+@app.route("/about", methods=["POST", "GET"])    
 def about_page():
         return render_template("about.html")
 
 #Homepage
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home_page():
         return render_template("index.html")
 
 #Results
-@app.route("/results", methods=["POST"])  
-def calculate_bpm():
+@app.route("/results", methods=["POST", "GET"])
+def results():
     form_data = request.form
-    
 #raw_input
     pace_minutes1 = int(form_data['pace_minutes1'])
     pace_seconds1 = int(form_data['pace_seconds1'])
@@ -58,7 +84,65 @@ def calculate_bpm():
     bpm = round(minute_pace)
     print (bpm)
 
-    return render_template("results.html", bpm=bpm, stride=stride_in_metres, km=form_data['desired_distance'], hours=form_data['desired_time_hours'], mins=form_data['desired_time_minutes'], seconds=form_data['desired_time_seconds'])
+    #parser = argparse.ArgumentParser(description='Creates a playlist for user')
+    
+    #parser.add_argument('-p', '--playlist', required=False, default='Cadence Playlist', help='Name of Playlist')
+    #parser.add_argument('-d', '--description', required=False, default='', help='Description of Playlist')
+    #args = parser.parse_args()
+    
+    #make playlist
+    scope = "playlist-modify-public user-library-read"
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    user_id = sp.me()['id']
+    playlist_name="Cadence Playlist"
+    playlist_description="Made for you by the Cadence App, to run {distance}km in {h}h{m}m{s}s. Keep to the beat!".format(
+        distance = desired_distance,
+        h = desired_time_hours,
+        m = desired_time_minutes,
+        s = desired_time_seconds
+        )
+    
+    user=user_id
+    name=playlist_name
+    description=playlist_description
+    playlist_owner_id=user_id
+    playlist_length=0
+
+    new_playlist= sp.user_playlist_create(user, name, public=True, collaborative=False, description=description)
+    playlist_id=new_playlist['id']
+    playlist_url=new_playlist['external_urls']['spotify']
+    
+    
+    def list_tracks():
+        library = sp.current_user_saved_tracks()
+        for idx, item in enumerate(library['items']):
+            track = item['track']
+            print(idx, track['artists'][0]['name'], " – ", track['name'], track['tempo'])
+            margin_of_error=bpm*0.1
+            if playlist_length<desired_time_in_seconds and item['tempo']<(bpm+margin_of_error) and item['tempo']>(bpm-margin_of_error):
+                items_list.append(idx)
+                playlist_length=playlist_length+(track[duration_ms]*1000)
+        items_list_out=items_list
+        print (list_tracks)
+        return list_tracks()
+    
+    playlist_message = "Here's your playlist!"
+    def fill_playlist():
+        items_list_out=items_list
+        sp.playlist_add_items(playlist_id, items_list_out, position=None)
+        if playlist_length<desired_time_in_seconds:
+            playlist_message = "On no! There is not enough music in your library to cover the whole run. You may have to put this playlist on repeat!"
+        else:
+            playlist_message = "What a great playlist - you have very good taste!"
+        print("Playlist length in seconds: ", playlist_length)
+        return fill_playlist()
+    
+    #list_tracks()
+    #fill_playlist()
+    return render_template("results.html", bpm=bpm, stride=stride_in_metres, km=form_data['desired_distance'], hours=form_data['desired_time_hours'], mins=form_data['desired_time_minutes'], seconds=form_data['desired_time_seconds'], playlist_id=new_playlist['id'], playlist_message=playlist_message)
+
+   # return sp.playlist_add_items(playlist_id, items)
 
 #debug
-app.run(debug=True) #runs the app. the debug part - unlocks debugging feature
+if __name__ == '__main__':
+    app.run(app.run(debug=True)) #runs the app. the debug part - unlocks debugging feature
