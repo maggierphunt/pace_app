2532c7b0eb420cfe0d5dcca836c885c9277e50d4
Maggie Hunt <50377783+maggierphunt@users.noreply.github.com>
2021-05-15 11:26:00 +0100

    js

--- static/js/script.js
+++ static/js/script.js
@@ -1,40 +1,18 @@
-function yesnoCheck(that) {
-
-    if (that.value == "Cadence") {
-        document.getElementById("perkm1").style.display = "block";
-        document.getElementById("perkm2").style.display = "block";
-        document.getElementById("perkm3").style.display = "block";
-        document.getElementById("ifCadence1").style.display = "block";
-        document.getElementById("ifCadence2").style.display = "block";
-        document.getElementById("ifCadence3").style.display = "block";
-        document.getElementById("ifDSteps1").style.display = "none";
-        document.getElementById("ifDSteps2").style.display = "none";
-        document.getElementById("ifDSteps3").style.display = "none";
-        document.getElementById("ifDistance1").style.display = "none";
-        document.getElementById("ifDistance2").style.display = "none";
-        document.getElementById("ifDistance3").style.display = "none";
-        document.getElementById("running1").style.display = "none";
-        document.getElementById("running2").style.display = "none";
-        document.getElementById("running3").style.display = "none";
-
-    } else {
-        document.getElementById("perkm1").style.display = "none";
-        document.getElementById("perkm2").style.display = "none";
-        document.getElementById("perkm3").style.display = "none";
-        document.getElementById("ifCadence1").style.display = "none";
-        document.getElementById("ifCadence2").style.display = "none";
-        document.getElementById("ifCadence3").style.display = "none";
-        document.getElementById("ifDistance1").style.display = "block";
-        document.getElementById("ifDistance2").style.display = "block";
-        document.getElementById("ifDistance3").style.display = "block";
-        document.getElementById("running1").style.display = "block";
-        document.getElementById("running2").style.display = "block";
-        document.getElementById("running3").style.display = "block";
-        document.getElementById("ifDSteps1").style.display = "block";
-        document.getElementById("ifDSteps2").style.display = "block";
-        document.getElementById("ifDSteps3").style.display = "block";
-    }
-    
+hideonLoad() {
+    document.getElementById("ifDSteps1").style.display = "none";
+    document.getElementById("ifDSteps2").style.display = "none";
+    document.getElementById("ifDSteps3").style.display = "none";
+    document.getElementById("ifDistance1").style.display = "none";
+    document.getElementById("ifDistance2").style.display = "none";
+    document.getElementById("ifDistance3").style.display = "none";
+    document.getElementById("running1").style.display = "none";
+    document.getElementById("running2").style.display = "none";
+    document.getElementById("running3").style.display = "none";
+    document.getElementById("perkm1").style.display = "none";
+    document.getElementById("perkm2").style.display = "none";
+    document.getElementById("perkm3").style.display = "none";
+    document.getElementById("ifCadence1").style.display = "none";
+    document.getElementById("ifCadence2").style.display = "none";
+    document.getElementById("ifCadence3").style.display = "none";
+    resolve();
 }
-
-
