eeefc1fb857c4d8ee8c0411bb8e8a1211ee85dc1
Maggie Hunt <50377783+maggierphunt@users.noreply.github.com>
2021-05-15 15:07:31 +0100

    javascript

--- run.gunicorn.sh
+++
@@ -1,1 +0,0 @@
-gunicorn -b :5000 --access-logfile - --error-logfile - pace_app:pace_app
---
+++ static/js/script.js
@@ -0,0 +1,19 @@
+
+hideonLoad() {
+    document.getElementById("ifDSteps1").style.display = "none";
+    document.getElementById("ifDSteps2").style.display = "none";
+    document.getElementById("ifDSteps3").style.display = "none";
+    document.getElementById("ifDistance1").style.display = "none";
+    document.getElementById("ifDistance2").style.display = "none";
+    document.getElementById("ifDistance3").style.display = "none";
+    document.getElementById("running1").style.display = "none";
+    document.getElementById("running2").style.display = "none";
+    document.getElementById("running3").style.display = "none";
+    document.getElementById("perkm1").style.display = "none";
+    document.getElementById("perkm2").style.display = "none";
+    document.getElementById("perkm3").style.display = "none";
+    document.getElementById("ifCadence1").style.display = "none";
+    document.getElementById("ifCadence2").style.display = "none";
+    document.getElementById("ifCadence3").style.display = "none";
+    resolve();
+}
